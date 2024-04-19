1f1d4d7d9dca93c5352d6805da9db88ef7658c1d
MarlonMQ <marlonmurilloquesada@hotmail.com>
2024-04-19 11:21:22 -0600

    call to funcion isTableSovable removed

--- app.py
+++ app.py
@@ -21,49 +21,49 @@ class App():
         print("solving the puzzle: ")
         print(selected_matrix)
         
-        if eightPuzzleSolver.isTableSolvable(selected_matrix) == False:
-            print("The puzzle is not solvable")
-        else:
-            print("The puzzle is solvable")
+        # if eightPuzzleSolver.isTableSolvable(selected_matrix) == False:
+        #     print("The puzzle is not solvable")
+        # else:
+        #     print("The puzzle is solvable")
 
             # Breadth First
-            if algorithmId == 0:
-                print("Executing breadthFirst")
-                startTime = time.time()
-                eightPuzzleSolver.breadthFirst(selected_matrix)
-                endTime = time.time()
-                print("Time elapsed in seconds: ", endTime - startTime)
-            
-            # Greedy
-            elif algorithmId == 1:
-                print("Executing greedy")
-                startTime = time.time()
-                eightPuzzleSolver.greedy(selected_matrix)
-                endTime = time.time()
-                print("Time elapsed in seconds: ", endTime - startTime)
-            
-            # IDS
-            elif algorithmId == 2:
-                print("Executing IDS")
-                startTime = time.time()
-                eightPuzzleSolver.IDS(selected_matrix)
-                endTime = time.time()
-                print("Time elapsed in seconds: ", endTime - startTime)
-            
-            # IDS star
-            elif algorithmId == 3:
-                print("Executing IDS star")
-                startTime = time.time()
-                eightPuzzleSolver.idsStar(selected_matrix)
-                endTime = time.time()
-                print("Time elapsed in seconds: ", endTime - startTime)
-            
-            # Invalid algorithm id
-            else:
-                print("Invalid algorithm id")
-                return
+        if algorithmId == 0:
+            print("Executing breadthFirst")
+            startTime = time.time()
+            eightPuzzleSolver.breadthFirst(selected_matrix)
+            endTime = time.time()
+            print("Time elapsed in seconds: ", endTime - startTime)
+        
+        # Greedy
+        elif algorithmId == 1:
+            print("Executing greedy")
+            startTime = time.time()
+            eightPuzzleSolver.greedy(selected_matrix)
+            endTime = time.time()
+            print("Time elapsed in seconds: ", endTime - startTime)
+        
+        # IDS
+        elif algorithmId == 2:
+            print("Executing IDS")
+            startTime = time.time()
+            eightPuzzleSolver.IDS(selected_matrix)
+            endTime = time.time()
+            print("Time elapsed in seconds: ", endTime - startTime)
+        
+        # IDS star
+        elif algorithmId == 3:
+            print("Executing IDS star")
+            startTime = time.time()
+            eightPuzzleSolver.idsStar(selected_matrix)
+            endTime = time.time()
+            print("Time elapsed in seconds: ", endTime - startTime)
+        
+        # Invalid algorithm id
+        else:
+            print("Invalid algorithm id")
+            return
 
-            # Get memory usage
-            process = psutil.Process()
-            memoryUsage = process.memory_info().rss  # in bytes
-            print("Memory Usage:", memoryUsage, "bytes")
+        # Get memory usage
+        process = psutil.Process()
+        memoryUsage = process.memory_info().rss  # in bytes
+        print("Memory Usage:", memoryUsage, "bytes")
