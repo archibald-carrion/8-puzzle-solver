fafa2f8e802a9da568ee037543182909a2233fcf
MarlonMQ <marlonmurilloquesada@hotmail.com>
2024-04-19 11:12:41 -0600

    matrix generator

--- matrices.py
+++ matrices.py
@@ -1,115 +1,109 @@
-# Dictionary containing the matrix variables from py
+# # Dictionary containing the matrix variables from py
 
-matrixSolvable0 = [[0, 8, 2],
-                     [1, 4, 3],
-                     [6, 5, 7]]
+# matrixSolvable0 = [[0, 8, 2],
+#                      [1, 4, 3],
+#                      [6, 5, 7]]
 
-matrixSolvable1 = [[1, 2, 3],
-                     [4, 8, 7],
-                     [6, 0, 5]]
+# matrixSolvable1 = [[1, 2, 3],
+#                      [4, 8, 7],
+#                      [6, 0, 5]]
                      
-matrixSolvable2 = [[1, 2, 3],
-                     [4, 6, 8],
-                     [7, 5, 0]]
+# matrixSolvable2 = [[1, 2, 3],
+#                      [4, 6, 8],
+#                      [7, 5, 0]]
 
-matrixSolvable3 = [[7, 3, 6],
-                     [5, 1, 2],
-                     [8, 0, 4]]
+# matrixSolvable3 = [[7, 3, 6],
+#                      [5, 1, 2],
+#                      [8, 0, 4]]
 
-matrixSolvable4 = [[1, 2, 5],
-                     [4, 0, 8],
-                     [6, 7, 3]]
+# matrixSolvable4 = [[1, 2, 5],
+#                      [4, 0, 8],
+#                      [6, 7, 3]]
 
-matrixSolvable5 = [[0, 1, 3],
-                     [8, 2, 7],
-                     [4, 6, 5]]
+# matrixSolvable5 = [[0, 1, 3],
+#                      [8, 2, 7],
+#                      [4, 6, 5]]
                      
-matrixSolvable6 = [[1, 3, 8],
-                     [7, 2, 4],
-                     [5, 6, 0]]
+# matrixSolvable6 = [[1, 3, 8],
+#                      [7, 2, 4],
+#                      [5, 6, 0]]
 
-matrixSolvable7 = [[4, 8, 1],
-                     [2, 0, 7],
-                     [6, 3, 5]]
+# matrixSolvable7 = [[4, 8, 1],
+#                      [2, 0, 7],
+#                      [6, 3, 5]]
 
-matrixSolvable8 = [[2, 8, 5],
-                     [6, 7, 0],
-                     [1, 3, 4]]
+# matrixSolvable8 = [[2, 8, 5],
+#                      [6, 7, 0],
+#                      [1, 3, 4]]
 
-matrixSolvable9 = [[0, 7, 6],
-                     [8, 4, 3],
-                     [2, 1, 5]]
+# matrixSolvable9 = [[0, 7, 6],
+#                      [8, 4, 3],
+#                      [2, 1, 5]]
                      
-matrixSolvable10 = [[4, 0, 7],
-                     [2, 5, 3],
-                     [6, 8, 1]]
+# matrixSolvable10 = [[4, 0, 7],
+#                      [2, 5, 3],
+#                      [6, 8, 1]]
 
-matrixSolvable11 = [[1, 5, 0],
-                     [3, 8, 2],
-                     [4, 6, 7]]
+# matrixSolvable11 = [[1, 5, 0],
+#                      [3, 8, 2],
+#                      [4, 6, 7]]
                      
-matrixSolvable12 = [[4, 3, 8],
-                     [1, 6, 0],
-                     [7, 2, 5]]
+# matrixSolvable12 = [[4, 3, 8],
+#                      [1, 6, 0],
+#                      [7, 2, 5]]
 
-matrixSolvable13 = [[2, 3, 6],
-                     [1, 7, 0],
-                     [8, 5, 4]]
+# matrixSolvable13 = [[2, 3, 6],
+#                      [1, 7, 0],
+#                      [8, 5, 4]]
 
-matrixSolvable14 = [[0, 2, 3],
-                     [7, 4, 5],
-                     [1, 6, 8]]
+# matrixSolvable14 = [[0, 2, 3],
+#                      [7, 4, 5],
+#                      [1, 6, 8]]
 
-matrixSolvable15 = [[1, 2, 5],
-                     [6, 0, 8],
-                     [3, 7, 4]]
+# matrixSolvable15 = [[1, 2, 5],
+#                      [6, 0, 8],
+#                      [3, 7, 4]]
                      
-matrixSolvable16 = [[0, 6, 4],
-                     [7, 8, 1],
-                     [3, 5, 2]]
+# matrixSolvable16 = [[0, 6, 4],
+#                      [7, 8, 1],
+#                      [3, 5, 2]]
 
-matrixSolvable17 = [[3, 0, 7],
-                     [4, 2, 5],
-                     [1, 8, 6]]
+# matrixSolvable17 = [[3, 0, 7],
+#                      [4, 2, 5],
+#                      [1, 8, 6]]
 
-matrixSolvable18 = [[7, 6, 1],
-                     [5, 0, 2],
-                     [3, 4, 8]]
+# matrixSolvable18 = [[7, 6, 1],
+#                      [5, 0, 2],
+#                      [3, 4, 8]]
 
-matrixSolvable19 = [[4, 2, 7],
-                     [8, 3, 1],
-                     [6, 5, 0]]
+# matrixSolvable19 = [[4, 2, 7],
+#                      [8, 3, 1],
+#                      [6, 5, 0]]
 
-# unsolvable matrix
-matrixUnsolvable1 = [[1, 7, 3],
-                       [0, 6, 2],
-                       [8, 4, 5]]
+# # unsolvable matrix
+# matrixUnsolvable1 = [[1, 7, 3],
+#                        [0, 6, 2],
+#                        [8, 4, 5]]
 
-matrixUnsolvable2 = [[1, 2, 3],
-                       [4, 5, 6],
-                       [8, 7, 0]]
+# matrixUnsolvable2 = [[1, 2, 3],
+#                        [4, 5, 6],
+#                        [8, 7, 0]]
 
+import random
 
+def generar_matriz_aleatoria():
+    numeros_disponibles = list(range(10))
+    matriz = [[None, None, None], [None, None, None], [None, None, None]]
+    
+    for i in range(3):
+        for j in range(3):
+            numero = random.choice(numeros_disponibles)
+            matriz[i][j] = numero
+            numeros_disponibles.remove(numero) 
+    
+    return matriz
 
-matricesMaping = {
-    0: matrixSolvable0,
-    1: matrixSolvable1,
-    2: matrixSolvable2,
-    3: matrixSolvable3,
-    4: matrixSolvable4,
-    5: matrixSolvable5,
-    6: matrixSolvable6,
-    7: matrixSolvable7,
-    8: matrixSolvable8,
-    9: matrixSolvable9,
-    10: matrixSolvable10,
-    11: matrixSolvable11,
-    12: matrixSolvable12,
-    13: matrixSolvable13,
-    14: matrixSolvable14,
-    15: matrixSolvable15,
-    16: matrixSolvable16,
-    17: matrixSolvable17,
-    18: matrixSolvable18,
-    19: matrixSolvable19
-}
+matricesMaping = {}
+for i in range(0, 20):
+    matriz_aleatoria = generar_matriz_aleatoria()
+    matricesMaping[i] = matriz_aleatoria
